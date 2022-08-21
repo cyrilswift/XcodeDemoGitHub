e4ac572c4e17dd8638691c3ffa9ebea41468b13c
Cyril F <cyrilfonrosecoding@gmail.com>
2022-08-21 11:39:51 -0400

    Initial Commit

---
+++ XcodeDemoGitHub/XcodeDemoGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeDemoGitHubApp.swift
+//  XcodeDemoGitHub
+//
+//  Created by Cyril Fonrose on 8/21/22.
+//
+
+import SwiftUI
+
+@main
+struct XcodeDemoGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
